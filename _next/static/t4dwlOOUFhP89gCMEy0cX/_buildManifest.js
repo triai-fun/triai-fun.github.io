a5acba13c7fadef8af3404921ac85f03b4d5b143
triai <triai@mail.com>
2025-09-19 06:59:06 +0000

    Update website files  - Fri Sep 19 06:59:05 UTC 2025

---
+++ _next/static/t4dwlOOUFhP89gCMEy0cX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-bede3a8288930dcf.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
